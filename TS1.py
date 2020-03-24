9f002558ab6b5fbd4800a13b7fdd382c991fc1eb
aurelien <kelekoaurelien@yahoo.fr>
2020-03-24 10:02:46 +0100

    debut code

---
+++ TS1.py
@@ -0,0 +1,19 @@
+
+# code du 22/03/2020
+# Serie temporelle
+
+#  aury kt
+
+'''An End-to-End Project on Time Series Analysis and Forecasting with Python
+'''
+
+# data: Superstore sales data
+
+# import library
+
+import warnings
+import itertools
+import numpy as np
+import pandas as pd
+import matplotlib
+import statsmodels.api as sm
